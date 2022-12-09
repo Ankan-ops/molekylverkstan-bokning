c9a4f76041df57eebf03e424b6eafeae07e90867
Nicolas Lundberg <nicolas.lundberg3@gmail.com>
2022-12-09 10:33:41 +0100

    more of booking

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database("db/databas.db");
 
+
+
+import React, {useState} from 'react';
+import BookingCalendar from 'booking-calendar';
+
+export default function App() {
+    const today = new Date()
+    const nextWeek =  new Date(today.getFullYear(), today.getMonth(), today.getDate()+7);
+    const [startDate, setStartDate] = useState(today);
+    const [endDate, setEndDate] = useState(nextWeek);
+
+    return (
+        <BookingCalendar 
+            defaultAvailable={true}
+            onSelectStart={setStartDate}
+            onSelectEnd={setEndDate}
+            bookingPickerType={'dateRangePicker'}
+        />
+    );
+
+}
